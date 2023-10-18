d897041e44d321c3553adeca92b9e684735d8fee
utf8-coding <1422346447@qq.com>
2023-10-18 11:55:40 +0800

    10.18

--- app/src/main/java/com/example/calculator/activities/MainActivity.kt
+++ app/src/main/java/com/example/calculator/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculator.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -37,6 +39,7 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculator.ui.theme.CalculatorTheme
@@ -63,13 +66,7 @@ fun calcString(textFieldString: String, onOutCome: (value: Double, error: Error?
 
 @Composable
 fun MainView() {
-    val textFieldValueState = remember {
-        mutableStateOf(
-            TextFieldValue(
-                text = ""
-            )
-        )
-    }
+    val textFieldValueState = remember { mutableStateOf("") }
     Surface(
         modifier =
         Modifier
@@ -90,28 +87,27 @@ fun MainView() {
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun NumDisplayCard(
-    textFieldState: MutableState<TextFieldValue>,
+    textFieldState: MutableState<String>,
     isPreview: Boolean = false
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     Surface(
-        color = MaterialTheme.colorScheme.primaryContainer,
         modifier = Modifier.padding(surfacePadding),
         shape = RoundedCornerShape(roundCornerSize),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.outline)
     ) {
         Column{
-            Spacer(modifier = Modifier.height(50.dp))
+            Spacer(modifier = Modifier.height(80.dp))
             BasicTextField(
-                modifier = Modifier.background(Color.Transparent),
+                modifier = Modifier.background(Color.Transparent).fillMaxWidth(),
                 textStyle = MaterialTheme.typography.displayLarge,
                 enabled = false,
                 singleLine = true,
-                value = if (isPreview) {
-                    TextFieldValue("1234, text")
+                value = if (isPreview) {"1234, text"
                 } else {
                     textFieldState.value
                 },
-                onValueChange = { value: TextFieldValue ->
+                onValueChange = { value: String ->
                     textFieldState.value = value
                 },
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
@@ -123,7 +119,7 @@ fun NumDisplayCard(
 
 @Composable
 fun KeyBoardView(
-    textFieldState: MutableState<TextFieldValue>
+    textFieldState: MutableState<String>
 ) {
     val funcBtnDispContent =
         arrayListOf(
@@ -143,7 +139,7 @@ fun KeyBoardView(
             "1", "2", "3", "-", "spc",
             ".", "0", "spc", "+", "="
         ) // 5*4 del, spc, = 是特殊标志，后文进行特殊处理
-    val numpadBtnOperationContent =
+    val numpadBtnOperationOperation =
         arrayListOf(
             "7", "8", "9", "÷", "del",
             "4", "5", "6", "×", "^",
@@ -152,6 +148,8 @@ fun KeyBoardView(
         )
 
     Column(
+        modifier = Modifier.background(Color.Transparent),
+
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
@@ -180,17 +178,17 @@ fun KeyBoardView(
                                             interactionSource = remember { MutableInteractionSource() },
                                             indication = rememberRipple(bounded = false),
                                             onClick = {
-                                                textFieldState.value.text.plus(funcBtnInptContent[j + i * 4])
+                                                textFieldState.value = textFieldState.value + (funcBtnInptContent[j + i * 4])
                                             }
                                         ),
                                 onClick = {}
                             ){
                                 Text(
                                     style = TextStyle(
-                                        fontSize = LocalDensity.current.run{18.dp.toSp()}
+                                        fontSize = LocalDensity.current.run{20.dp.toSp()},
+                                        color = MaterialTheme.typography.displayMedium.color
                                     ),
-                                    text = funcBtnDispContent[j+i*4],
-                                    color = MaterialTheme.colorScheme.secondary
+                                    text = funcBtnDispContent[j+i*4]
                                 )
                             }
                         }
@@ -211,36 +209,46 @@ fun KeyBoardView(
                 for(i in 0..3){
                     Row(
                         modifier = Modifier
-                            .padding(vertical = surfacePadding / 2)
+                            .padding(vertical = surfacePadding *2)
                             .fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceEvenly,
                     ){
                         for(j in 0..4){
-                            Button(
-                                colors = ButtonDefaults.buttonColors(
-                                    containerColor = MaterialTheme.colorScheme.secondaryContainer
-                                ),
-                                modifier =
-                                Modifier
-                                    .clickable(
-                                        interactionSource = remember { MutableInteractionSource() },
-                                        indication = rememberRipple(bounded = false),
-                                        onClick = {
-                                            textFieldState.value.text.plus(numpadBtnOperationContent[j+i*5])
-                                        }
-                                    )
-                                    .weight(20f),
-                                onClick = {}
-                            ){
-                                Text(
-                                    style = TextStyle(
-                                        fontSize = LocalDensity.current.run{20.dp.toSp()},
+                            if(numpadBtnDispContent[j+i*5] == "spc"){
+                                Spacer(Modifier.weight(20f))
+                            } else{
+                                Surface(
+                                    modifier =
+                                    Modifier
+                                        .clickable(
+                                            interactionSource = remember { MutableInteractionSource() },
+                                            indication = rememberRipple(bounded = false),
+                                            onClick = {
+                                                if(numpadBtnOperationOperation[j+i*5] == "del"){
+                                                    textFieldState.value.removeRange(textFieldState.value.length - 1 until textFieldState.value.length)
+                                                }else if (numpadBtnOperationOperation[j+i*5] == "="){
 
-                                    ),
-                                    text = numpadBtnOperationContent[j+i*5],
-                                    color = MaterialTheme.colorScheme.secondary
-                                )
+                                                }else {
+                                                    textFieldState.value = textFieldState.value + numpadBtnOperationOperation[j+i*5]
+                                                }
+                                            }
+                                        )
+                                        .weight(20f)
+                                        .padding(horizontal = 5.dp)
+                                ){
+                                    Text(
+                                        modifier = Modifier.background(Color.Transparent),
+                                        style = TextStyle(
+                                            fontSize = LocalDensity.current.run{30.dp.toSp()},
+                                            color = MaterialTheme.typography.displayMedium.color,
+                                            textAlign = TextAlign.Center
+                                            ),
+                                        text = numpadBtnDispContent[j+i*5],
+                                        color = MaterialTheme.typography.bodyMedium.color
+                                    )
+                                }
                             }
+
                         }
                     }
                 }
